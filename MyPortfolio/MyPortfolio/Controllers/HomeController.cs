aa1dc944a832d615ad2f5212440cb4b43e14657f
taekionic <98569426+taekionic@users.noreply.github.com>
2022-07-08 17:13:16 -0400

    added files

---
+++ MyPortfolio/MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Fluent.Infrastructure.FluentModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyPortfolio/MyPortfolio/wwwroot/js/site.js
@@ -0,0 +1,177 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//let currentPage = location.href; // store url on load
+
+//setInterval(function () {
+//    if (currentPage != location.href) {
+//        currentPage = location.href;
+//    }
+//}, 500);
+
+
+//if (location.href.path = '/Home/Projects') { }
+
+const menu_toggle = document.querySelector('.menu_toggle');
+const navigation = document.querySelector('.navigation');
+menu_toggle.onclick = function () {
+    navigation.classList.toggle('active');
+}
+
+var windowsize = $(window).width();
+
+$(window).resize(function () {
+    var windowsize = $(window).width();
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    $('.navigation').css({
+        "transform": "translatex(0%)",
+    })
+})
+
+$(".navlink").on('click', function () {
+    var url = $(this).attr('href');
+    if (window.location.pathname == url) {
+        window.location.reload()
+    }
+    else {
+        $('.navigation').css("transform", "translatex(-100%)")
+    }
+})
+
+if (windowsize < 1150) {
+    //if the window is greater than 440px wide then turn on jScrollPane..
+    $('.images').remove()
+}           
+
+setTimeout(function () {
+    document.getElementById('instruction').style.transition = '1s ease-out'
+    document.getElementById('instruction').style.transform = 'scale(0)'
+    setTimeout(function () { document.getElementById('instruction').remove() }, 6000)
+}, 5000)
+
+
+//var counter = 0
+const margin = 96
+
+
+    $(".portfoliocard").on('click', function () {
+        //console.log($(this).index())
+        $('.portfoliocard span').css("display", "none")
+        $('.portfoliocard section').css("display", "none")
+        $('.images').hide()
+        var index = ($(this).index())
+
+        $(this).siblings('.portfoliocard').css({
+            "left": "0",
+            "top": "30vh",
+            "z-index": "2"
+        })
+        $(this).css({
+            "left": "0",
+            "top": "30vh",
+            "z-index": "2"
+        });
+        $(this).addClass('hovered');
+        $(this).siblings('.portfoliocard').addClass('hovered')
+        $('#instruction').css("display", "none")
+
+        $('#cardview').css({
+            "transition": "1s ease-in",
+            "max-width": "1oovw",
+            "overflow": "hidden"
+        })
+
+        $(this).children('.portfoliocontent').css({
+            "cursor": "auto"
+        }, 1000)
+
+
+        $(".returnbutton").on('click', function () {
+            $('.portfoliowrapper').css({
+                "transform": "500vw",
+            })
+            setTimeout(function () {
+                window.location.reload();
+            }, 500)
+
+        })
+
+        var currentaxis = (-($(this).index()) * 96)
+
+
+        $('.nextbutton').on('click', function () {
+            $('.nextbutton').data('clicked', true)
+            nextaxis = currentaxis - 96
+            currentaxis = nextaxis
+                $('.portfoliocard').css({
+                    "transform": "translate(" + currentaxis + "vw)"
+                })
+                return currentaxis
+            })
+
+        $('.prevbutton').on('click', function () {
+            $('.prevbutton').data('clicked', true)
+            prevaxis = currentaxis + 96
+            currentaxis = prevaxis
+                $('.portfoliocard').css({
+                    "transform": "translate(" + currentaxis + "vw)"
+                })
+                return prevaxis
+        })
+
+        if ($('.nextbutton').data('clicked')) {
+            $('.portfoliocard').css({
+                "transform": "translate(" + nextaxis + "vw)"
+            })
+            $('.nextbutton').data('clicked', false)
+        }
+        else if ($('.prevbutton').data('clicked')) {
+            $('.portfoliocard').css({
+                "transform": "translate(" + prevaxis + "vw)"
+            })
+            $('.prevbutton').data('clicked', false)
+        }
+        else {
+            $('.portfoliocard').css({
+                "transform": "translate(" + currentaxis + "vw)"
+            })
+        }
+    })
+
+
+
+
+
+
+    const slider = document.getElementById('portfolio');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+        slider.style.cursor = 'grabbing';
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+        slider.style.cursor = 'grab'
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+        console.log(walk);
+    })
